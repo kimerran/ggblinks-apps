f9ef44afd62fd0ab181303246295e949267a5d18
Mark Hugh Neri <mh.neri@gmail.com>
2024-09-04 18:36:36 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AppWalletProvider from "./components/AppWalletProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+    <head>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="ggtag" content="ggme:k1merran.sol" />
+    <meta name="dscvr:canvas:version" content="vNext" />
+    <meta name="description" content="Convert your social-media posts into Solana-powered Blinks. Send SOL, SEND, PYUSD tokens, mint NFTs, participate in airdrops and many more in the future!" />
+    <meta name="og:description" content="Convert your social-media posts into Solana-powered Blinks. Send SOL, SEND, PYUSD tokens, mint NFTs, participate in airdrops and many more in the future!" />
+    <meta name="twitter:description" content="Convert your social-media posts into Solana-powered Blinks. Send SOL, SEND, PYUSD tokens, mint NFTs, participate in airdrops and many more in the future!" />
+    <meta name="og:image" content="https://i.imgur.com/CXh9lkt.jpeg" />
+    <meta name="twitter:image" content="https://i.imgur.com/CXh9lkt.jpeg" />
+
+    <title>GGBlinks</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
+
+</head>
+      <body className={inter.className}>
+      <AppWalletProvider>{children}</AppWalletProvider>
+</body>
     </html>
   );
 }
